94958a2637db5c66f9ee33e6d873326e3553ae23
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-04-16 17:41:41 +0200

    inserted AlphaBeta-Pruning pseudocode

--- PlayerForTeam05.java
+++ PlayerForTeam05.java
@@ -27,5 +27,31 @@ public class PlayerForTeam05 extends Player{
 
     private int evaluateNode(int column, char[][] board){ return 0;}
 
-    private int alphabeta(int column, char[][] board, int depth, int alpha, int beta, Player maximizingPlayer){ return 0;}
+    private int alphabeta(int column, char[][] board, int depth, int alpha, int beta, boolean maximizingPlayer) {
+        if (depth == 0 || columnIsFull(board, column)) {
+            return evaluateNode(column, board);
+        }
+        if (maximizingPlayer){
+            for (int i = 0; i < board.length; i++){
+                alpha = Math.max(alpha, alphabeta(i, board, depth-1, alpha, beta, false));
+                if (beta <= alpha){
+                    break;
+                }
+            return alpha;
+            }
+        }
+        else {
+            for (int i = 0; i < board.length; i++){
+                beta = Math.min(beta, alphabeta(i, board, depth-1, alpha, beta, true));
+                if (beta <= alpha){
+                    break;
+                }
+            }
+            return beta;
+        }
+    }
+
+    private boolean columnIsFull(final char[][] board, final int column){
+        return false;
+    }
 }
