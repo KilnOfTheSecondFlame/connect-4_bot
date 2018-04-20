8b0952ae0a82c9d85a0f9a2d44b3914d67cc2661
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-04-20 14:22:43 +0200

    Added Setup to Test, removed test for private method

--- test/PlayerForTeam05Test.java
+++ test/PlayerForTeam05Test.java
@@ -1,8 +1,17 @@
+import ch.hslu.ai.connect4.Player;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class PlayerForTeam05Test {
-    @Test
-    public void testColumnIsFull(){
 
+    private static final int ROWS = 6;
+    private static final int COLUMNS = 7;
+    private Player player;
+    private char[][] board;
+
+    @BeforeClass
+    public void setup() {
+        this.player = new PlayerForTeam05();
+        this.board = new char[COLUMNS][ROWS];
     }
 }
