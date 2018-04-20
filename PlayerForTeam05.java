5a3f93e422766e4bbf0d0fbd2118014b148faf55
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-04-20 14:24:42 +0200

    Added columnIsFull and TODO Let checkstyle run

--- PlayerForTeam05.java
+++ PlayerForTeam05.java
@@ -1,9 +1,9 @@
 import ch.hslu.ai.connect4.Player;
 
 
-public class PlayerForTeam05 extends Player{
+public class PlayerForTeam05 extends Player {
 
-    public PlayerForTeam05(){
+    public PlayerForTeam05() {
         super("Player for Team05");
     }
 
@@ -11,11 +11,12 @@ public class PlayerForTeam05 extends Player{
      * The following method allows you to implement your own game intelligence.
      * At the moment, this is a dumb random number generator.
      * The method must return the column number where the computer player puts the next disc.
-     * board[i][j] = cell content at position (i,j), i = column, j = row
-     *
-     * If board[i][j] = this.getSymbol(), the cell contains one of your discs
-     * If board[i][j] = '-', the cell is empty
+     * board[column][row] = cell content at position (column, row)
+     * <p>
+     * If board[column][row] = this.getSymbol(), the cell contains one of your discs
+     * If board[column][row] = '-', the cell is empty
      * Otherwise, the cell contains one of your opponent's discs
+     *
      * @param board The current game board
      * @return The columns number where you want to put your disc
      */
@@ -25,25 +26,27 @@ public class PlayerForTeam05 extends Player{
         return 0;
     }
 
-    private int evaluateNode(int column, char[][] board){ return 0;}
+    private int evaluateNode(int column, char[][] board) {
+        // TODO Heuristics go in here
+        return 0;
+    }
 
     private int alphabeta(int column, char[][] board, int depth, int alpha, int beta, boolean maximizingPlayer) {
         if (depth == 0 || columnIsFull(board, column)) {
             return evaluateNode(column, board);
         }
-        if (maximizingPlayer){
-            for (int i = 0; i < board.length; i++){
-                alpha = Math.max(alpha, alphabeta(i, board, depth-1, alpha, beta, false));
-                if (beta <= alpha){
+        if (maximizingPlayer) {
+            for (int i = 0; i < board.length; i++) {
+                alpha = Math.max(alpha, alphabeta(i, board, depth - 1, alpha, beta, false));
+                if (beta <= alpha) {
                     break;
                 }
-            return alpha;
+                return alpha;
             }
-        }
-        else {
-            for (int i = 0; i < board.length; i++){
-                beta = Math.min(beta, alphabeta(i, board, depth-1, alpha, beta, true));
-                if (beta <= alpha){
+        } else {
+            for (int i = 0; i < board.length; i++) {
+                beta = Math.min(beta, alphabeta(i, board, depth - 1, alpha, beta, true));
+                if (beta <= alpha) {
                     break;
                 }
             }
@@ -52,7 +55,8 @@ public class PlayerForTeam05 extends Player{
         return 0;
     }
 
-    private boolean columnIsFull(final char[][] board, final int column){
+    private boolean columnIsFull(final char[][] board, final int column) {
+        if (board[column][board[column].length - 1].equals('-')) return true;
         return false;
     }
 }
