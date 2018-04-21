7a8470e91f03aeae9e0a61e1dad0af1eaa8a2ecc
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-04-21 15:49:42 +0200

    Implemented method play

--- PlayerForTeam05.java
+++ PlayerForTeam05.java
@@ -1,7 +1,13 @@
 import ch.hslu.ai.connect4.Player;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 
 public class PlayerForTeam05 extends Player {
+
+    private final Double inf = Double.POSITIVE_INFINITY;
 
     public PlayerForTeam05() {
         super("Player for Team05");
@@ -22,16 +28,24 @@ public class PlayerForTeam05 extends Player {
      */
 
     @Override
-    public int play(char[][] board) {
-        return 0;
+    public int play(final char[][] board) {
+        List<Double> alphabetas = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            alphabetas.add(alphabeta(i, board, 3, -inf, inf, true));
+        }
+        return alphabetas.indexOf(Collections.max(alphabetas));
     }
 
-    private int evaluateNode(int column, char[][] board) {
+    private double evaluateNode(int column, char[][] board) {
         // TODO Heuristics go in here
         return 0;
     }
 
-    private int alphabeta(int column, char[][] board, int depth, int alpha, int beta, boolean maximizingPlayer) {
+    private double possibleFoursInThisLine(final char[] line) {
+        return 0;
+    }
+
+    private double alphabeta(int column, char[][] board, int depth, double alpha, double beta, boolean maximizingPlayer) {
         if (depth == 0 || columnIsFull(board, column)) {
             return evaluateNode(column, board);
         }
@@ -56,7 +70,7 @@ public class PlayerForTeam05 extends Player {
     }
 
     private boolean columnIsFull(final char[][] board, final int column) {
-        if (board[column][board[column].length - 1].equals('-')) return true;
-        return false;
+        if (board[column][board[column].length - 1] == ('-')) return false;
+        return true;
     }
 }
